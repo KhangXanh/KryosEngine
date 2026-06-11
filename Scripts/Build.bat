2c58d7b6f93a6aa995e6076c67daeb7355088422
unknown <khangphilip2805@gmail.com>
2026-06-11 20:09:30 +0700

    Update CMakeList.txt with submodules

--- Scripts/Build.bat
+++ Scripts/Build.bat
@@ -3,12 +3,12 @@
 cd /d "%~dp0"
 echo [Kryos Engine] Building...
 
-cmake --build Build
+cmake --build ..\Build
 if %errorlevel% neq 0 (
     echo [ERROR] Build failed!
     pause
     exit /b 1
 )
 
-echo [SUCCESS] Build complete! You can go to 'build\KryosStudio.exe' to use Kryos Engine.
+echo [SUCCESS] Build complete! You can go to 'Build\bin\KryosStudio.exe' to use Kryos Engine.
 pause
